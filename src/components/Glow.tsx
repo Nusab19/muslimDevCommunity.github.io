a6e82f5f697a5f3d17cc98d14947f1477e5ed349
Nusab Taha <nusabtaha33@gmail.com>
2025-02-27 21:26:50 +0600

    Background glow

---
+++ src/components/Glow.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface GlowProps {
+  colors?: string[];
+  opacity?: number;
+  className?: string;
+  blur?: string;
+  type?: string;
+}
+
+const Glow: React.FC<GlowProps> = ({
+  colors = ["#219ebc", "#003049", "#ffd166"],
+  opacity = 0.3,
+  blur = "40px",
+  className = "",
+  type = "ellipse",
+}) => {
+  // return <></>
+  return (
+    <div
+      className={`pointer-events-none absolute overflow-hidden ${className}`}
+      style={{
+        background: `radial-gradient(${type}, ${colors
+          .map((color, index) => `${color} ${index * 30}%`)
+          .join(", ")})`,
+        opacity: opacity,
+        filter: `blur(${blur})`,
+        mixBlendMode: "screen",
+      }}
+    />
+  );
+};
+
+export default Glow;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Noto_Naskh_Arabic } from "next/font/google";
 import { Button } from "./ui/button";
+import Glow from "./Glow";
 
 const notoNaskhArabic = Noto_Naskh_Arabic({
   subsets: ["arabic"],
@@ -10,20 +11,70 @@ const notoNaskhArabic = Noto_Naskh_Arabic({
 const Hero = () => {
   return (
     <section className="container mx-auto px-4 py-16 text-center">
-      <h2
-        className={`-mt-10 text-xl md:text-2xl font-bold mb-6 text-accent tracking-wide ${notoNaskhArabic.className}`}
-      >
-        بسم الله الرحمن الرحيم
-      </h2>
-      <h1 className="text-4xl font-bold mb-4 text-foreground">
-        Muslim Developers Community
-      </h1>
-      <p className="text-xl mb-8 text-foreground/80">
-        Connect, Learn, and Grow with Fellow Muslim Developers
-      </p>
-      <Button className="bg-accent text-accent-foreground hover:bg-accent/90 font-medium">
-        Join the Community
-      </Button>
+      <div className="hidden lg:block">
+        <Glow
+          blur="5vw"
+          type="ellipse at top"
+          colors={["#219ebc", "#219ebc"]}
+          opacity={0.15}
+          className="-top-12 right-0 h-[23rem] w-[43rem]"
+        />
+        <Glow
+          blur="4vw"
+          type="ellipse at top"
+          colors={["#231942", "#231942"]}
+          opacity={0.5}
+          className="-top-32 left-12 h-[23rem] w-[33rem] translate-x-1/2"
+        />
+        <Glow
+          blur="5vw"
+          type="ellipse at top"
+          colors={["#000000", "#000000"]}
+          opacity={0.9}
+          className="-top-10 left-1/2 h-[23rem] w-[33rem] -translate-x-1/2"
+        />
+      </div>
+
+      <div className="block lg:hidden">
+        <Glow
+          blur="9vw"
+          type="ellipse at top"
+          colors={["#000000", "#000000"]}
+          opacity={0.9}
+          className="left-1/2 top-10 h-[23rem] w-[60vw] -translate-x-1/2"
+        />
+        <Glow
+          blur="7vw"
+          type="ellipse at top"
+          colors={["#219ebc", "#219ebc"]}
+          opacity={0.1}
+          className="left-0 top-20 h-[15rem] w-[60vw]"
+        />
+        <Glow
+          blur="5vw"
+          type="ellipse at top"
+          colors={["#231942", "#231942"]}
+          opacity={0.3}
+          className="-top-32 right-0 h-[23rem] w-[60vw]"
+        />
+      </div>
+
+      <div className="z-50">
+        <h2
+          className={`-mt-10 mb-6 text-xl font-bold tracking-wide text-accent md:text-2xl ${notoNaskhArabic.className}`}
+        >
+          بسم الله الرحمن الرحيم
+        </h2>
+        <h1 className="mb-4 text-4xl font-bold text-foreground">
+          Muslim Developers Community
+        </h1>
+        <p className="mb-8 text-xl text-foreground/80">
+          Connect, Learn, and Grow with Fellow Muslim Developers
+        </p>
+        <Button className="bg-accent font-medium text-accent-foreground hover:bg-accent/90">
+          Join the Community
+        </Button>
+      </div>
     </section>
   );
 };
